81205364370efec4b34f7f54a04fc248ccb9daa5
Gabor Molnar <gabormolnar92@gmail.com>
2017-01-31 23:14:14 +0100

    Add mousetrap

--- src/js/UserInteraction.js
+++ src/js/UserInteraction.js
@@ -1,3 +1,5 @@
+import * as Mousetrap from 'mousetrap';
+
 export class UserInteraction{
 
 	/**
@@ -8,78 +10,33 @@ export class UserInteraction{
 	 */
 	constructor(editor){
 		this._editor = editor;
+		this._mousetrap = new Mousetrap.default(this._editor.getEditor);
 	}
 
 	/**
 	 *
 	 * @param key
 	 * @param prefix
-	 * @param value
-	 * @param postfix
+	 * @param suffix
 	 */
-	addKeyDownInsertContent(key,prefix = '',postfix = ''){
-		this._editor.addEventListener('keydown',(e)=>{
+	addKeyDownInsertContent(key,prefix = '',suffix = ''){
+
+		this._mousetrap.bind(key, () => console.log(key));
+
+		/*this._editor.addEventListener('keydown',(e)=>{
 			if(UserInteraction.pressKey(key,e)){
 				e.preventDefault();
 				let actualPosition = this._editor.getCursorPosition();
 				let value = this._editor.getSelectedContent();
-				this._editor.setSelectedContent(prefix+value+postfix);
+				this._editor.setSelectedContent(prefix+value+suffix);
 				if(value.length == 0){
 					this._editor.setCursorPosition(actualPosition+prefix.length);
 				} else {
-					this._editor.setCursorPosition(actualPosition+(prefix+value+postfix).length);
+					this._editor.setCursorPosition(actualPosition+(prefix+value+suffix).length);
 				}
 				this._editor.processContent();
 			}
-		});
-	}
-
-	/**
-	 *
-	 * @param keys
-	 * @param event
-	 * @returns {boolean}
-	 */
-	static pressKey(keys,event){
-		let isPressed = true;
-		for(let key of UserInteraction.parseKey(keys)){
-			isPressed &= UserInteraction.isKeyPressed(key,event);
-		}
-		return isPressed;
-	}
-
-	/**
-	 *
-	 * @param key
-	 * @returns {Array}
-	 */
-	static parseKey(key){
-		return key.split('+');
-	}
-
-	/**
-	 *
-	 * @param key
-	 * @param event
-	 * @returns {boolean}
-	 */
-	static isKeyPressed(key,event){
-		switch (key.toLowerCase()){
-		case 'ctrl':
-			return event.ctrlKey;
-		case 'alt':
-			return event.altKey;
-		case 'shift':
-			return event.shiftKey;
-		case 'tab':
-		case 'enter':
-		case 'capslock':
-		case 'backspace':
-		case 'space':
-			return event.code.toLowerCase() === key.toLowerCase();
-		default:
-			return event.code === 'Key'+key.toUpperCase();
-		}
+		});*/
 	}
 
 }
